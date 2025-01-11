892a38328a6a64ea765efed86b462dabe85cc2dd
ssued <ssued24@gmail.com>
2025-01-11 18:01:24 -0500

    Continued working on PyTorch Learning

--- CNN_steps/jobs/create_dagman.py
+++ CNN_steps/jobs/create_dagman.py
@@ -9,23 +9,23 @@ import os
 
 os.chdir('/data/i3home/ssued/RNOGCnn/CNN_steps/jobs')
 
-sym_num = 1 # Number of simulations to run 
+sym_num = 4 # Number of simulations to run 
 
 # now we have to write the dag file itself
 dag_filename = f"dagman.dag"
 instructions = ""
 #instructions += 'CONFIG config.dagman \n' Im not sure this is necessary
 
-instructions += f'JOB step1_0 step1.sub \n'
-instructions += f'JOB step2_0 step2.sub \n'
-instructions += f'VARS step2_0 inputfilename="1e19_n1e3.hdf5" detectordescription="station.json" config="config.yaml" outputfilename="output.hdf5" outputfilenameNuRadioReco="output.nur" \n'
-instructions += f'JOB step3_0 step3.sub \n\n'
-instructions += f'PARENT step1_0 CHILD step2_0\n'
-instructions += f'PARENT step2_0 CHILD step3_0\n\n'
+instructions += f'JOB stepOne_0 step1.sub \n'
+instructions += f'JOB stepTwo_0 step2.sub \n'
+instructions += f'VARS stepTwo_0 inputfilename="1e19_n1e3.hdf5" detectordescription="station.json" config="config.yaml" outputfilename="output.hdf5" outputfilenameNuRadioReco="output.nur" \n'
+instructions += f'JOB stepThree_0 step3.sub \n\n'
+instructions += f'PARENT stepOne_0 CHILD stepTwo_0\n'
+instructions += f'PARENT stepTwo_0 CHILD stepThree_0\n\n'
 
 if sym_num == 1:
-    instructions += f'JOB step4 step4.sub \n'
-    instructions += f'PARENT step3_0 CHILD step4'
+    instructions += f'JOB stepFour step4.sub \n'
+    instructions += f'PARENT stepThree_0 CHILD stepFour'
     with open(dag_filename, 'w') as fwrite:
         fwrite.write(instructions)
     sys.exit()
@@ -33,23 +33,24 @@ if sym_num == 1:
 sym_num = sym_num - 1 # shh, dont tell anyone
 
 for i in range(1, sym_num):
-    instructions += f'JOB step1_{i} step1.sub \n'
-    instructions += f'JOB step2_{i} step2.sub \n'
-    instructions += f'VARS step2_{i} inputfilename="1e19_n1e3.hdf5" detectordescription="station.json" config="config.yaml" outputfilename="output.hdf5" outputfilenameNuRadioReco="output.nur" \n'
-    instructions += f'JOB step3_{i} step3.sub \n\n'
-    instructions += f'PARENT step3_{i-1} CHILD step1_{i} \n'
-    instructions += f'PARENT step1_{i} CHILD step2_{i} \n'
-    instructions += f'PARENT step2_{i} CHILD step3_{i} \n\n'
+    instructions += f'JOB stepOne_{i} step1.sub \n'
+    instructions += f'JOB stepTwo_{i} step2.sub \n'
+    instructions += f'VARS stepTwo_{i} inputfilename="1e19_n1e3.hdf5" detectordescription="station.json" config="config.yaml" outputfilename="output.hdf5" outputfilenameNuRadioReco="output.nur" \n'
+    instructions += f'JOB stepThree_{i} step3.sub \n\n'
+    #instructions += f'PARENT stepThree_{i-1} CHILD stepOne_{i} \n'
+    instructions += f'PARENT stepOne_{i} CHILD stepTwo_{i} \n'
+    instructions += f'PARENT stepTwo_{i} CHILD stepThree_{i} \n\n'
 
-instructions += f'JOB step1_{sym_num} step1.sub \n'
-instructions += f'JOB step2_{sym_num} step2.sub \n'
-instructions += f'VARS step2_{sym_num} inputfilename="1e19_n1e3.hdf5" detectordescription="station.json" config="config.yaml" outputfilename="output.hdf5" outputfilenameNuRadioReco="output.nur" \n'
-instructions += f'JOB step3_{sym_num} step3.sub \n'
-instructions += f'JOB step4 step4.sub \n\n'
-instructions += f'PARENT step3_{sym_num-1} CHILD step1_{sym_num} \n'
-instructions += f'PARENT step1_{sym_num} CHILD step2_{sym_num}\n'
-instructions += f'PARENT step2_{sym_num} CHILD step3_{sym_num}\n'
-instructions += f'PARENT step3_{sym_num} CHILD step4\n'
+instructions += f'JOB stepOne_{sym_num} step1.sub \n'
+instructions += f'JOB stepTwo_{sym_num} step2.sub \n'
+instructions += f'VARS stepTwo_{sym_num} inputfilename="1e19_n1e3.hdf5" detectordescription="station.json" config="config.yaml" outputfilename="output.hdf5" outputfilenameNuRadioReco="output.nur" \n'
+instructions += f'JOB stepThree_{sym_num} step3.sub \n'
+instructions += f'JOB stepFour step4.sub \n\n'
+#instructions += f'PARENT stepThree_{sym_num-1} CHILD stepOne_{sym_num} \n'
+instructions += f'PARENT stepOne_{sym_num} CHILD stepTwo_{sym_num}\n'
+instructions += f'PARENT stepTwo_{sym_num} CHILD stepThree_{sym_num}\n\n'
+for i in range(1, sym_num+1):
+    instructions += f'PARENT stepThree_{i} CHILD stepFour\n'
 
 with open(dag_filename, 'w') as fwrite:
     fwrite.write(instructions)
